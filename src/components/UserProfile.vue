00ef9b9db6c8d06da2fcdc708c830ff1b92226fe
PauliusU <48020370+PauliusU@users.noreply.github.com>
2020-12-30 13:43:36 +0200

    Display tweets in UserProfile component, add TweetItem component

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -2,16 +2,25 @@
     <div class="user-profile">
         <div class="user-profile__user-panel">
             <h1 class="user-profile__username">@{{user.username}}</h1>
+            <div class="user-profile__admin-badge" v-if="user.isAdmin">
+                Admin
+            </div>
             <div class="user-profile__follower-count">
                 <strong>Followers: </strong> {{followers}}
             </div>
+        </div>
+        <div class="user-profile__tweets-wrapper">
+            <TweetItem class="user-profile__tweet" v-for="tweet in user.tweets" :key="tweet.id" :username="user.username" :tweet="tweet"/>
         </div>
     </div>
 </template>
 
 <script>
+    import TweetItem from "./TweetItem";
+
     export default {
         name: 'UserProfile',
+        components: {TweetItem},
         data() {
             return {
                 followers: 0,
@@ -21,7 +30,11 @@
                     firstName: "John",
                     lastName: "Smith",
                     email: "john@smith.com",
-                    isAdmin: true
+                    isAdmin: true,
+                    tweets: [
+                        {id: 1, content: "Twitter is Amazing!"},
+                        {id: 2, content: "Don't forget to subscribe to @JohnSmith1358"},
+                    ]
                 }
             }
         },
@@ -49,24 +62,39 @@
 </script>
 
 <style>
-.user-profile {
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    width: 100%;
-    padding: 50px 5%;
-}
+    .user-profile {
+        display: grid;
+        grid-template-columns: 1fr 3fr;
+        width: 100%;
+        padding: 50px 5%;
+    }
 
-.user-profile__user-panel {
-    display: flex;
-    flex-direction: column;
-    margin-right: 50px;
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E8;
-}
+    .user-profile__user-panel {
+        display: flex;
+        flex-direction: column;
+        margin-right: 50px;
+        padding: 20px;
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid #DFE3E8;
+    }
 
-h1 {
-    margin: 0;
-}
+    .user-profile__admin-badge {
+        background: rebeccapurple;
+        color: white;
+        border-radius: 5px;
+        margin-right: auto;
+        padding: 0 10px;
+        font-weight: bold;
+    }
+
+    h1 {
+        margin: 0;
+    }
+
+    .user-profile__tweets-wrapper {
+        display: grid;
+        grid-gap: 10px;
+        margin-bottom: auto;
+    }
 </style>
